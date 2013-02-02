b934a54de92d021cb879824171711795a6479a0f
Vaclav Klecanda <vencax77@gmail.com>
2013-02-02 15:17:58 +0100

    logging stuff

--- projectgroup_command_runner/processor.py
+++ projectgroup_command_runner/processor.py
@@ -16,6 +16,11 @@ class CommandProcessor(Settings):
     def __init__(self, desired_config_part):
         self.desired_config_part = desired_config_part
         self.load_config()
+        
+    def process_specific_settings(self, cfgMod):
+        logfile = getattr(cfgMod, 'COMMANDLOGFILE', None)
+        level = logging._levelNames[getattr(cfgMod, 'LOGLEVEL', 'WARN')]
+        logging.basicConfig(filename=logfile, level=level)
 
     def process_project_settins(self, proj_sett, proj_path):
         try:
@@ -31,7 +36,9 @@ class CommandProcessor(Settings):
         """
         python_binary = os.path.join(proj_path, self.path_to_python)
         manage = os.path.join(proj_path, self.path_to_manage, 'manage.py')
-        self.call([python_binary, manage, commandToRun, params])
+        toCall = [python_binary, manage, commandToRun, params]
+        logging.debug('calling: %s' % toCall)
+        self.call(toCall)
 
     def call(self, called):
         try:
